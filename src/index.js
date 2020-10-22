598416b7bd73208cba64dc3b0e28b94cba11bba1
orest-forest <danilchenko.orest@gmail.com>
2020-10-22 21:22:22 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = n + '';
+    let arr = str.split('');
+    let newArr = [];
+    if (arr[0] === '-') {
+        arr.splice(0, 1);
+    }
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        newArr.push(arr[i])
+    }
+
+    newArr = newArr.join('');
+    return +newArr
+};
